941fd2398805b3690e9d0e945212bdc60dd7871b
APIVerve Team <hello@apiverve.com>
2024-05-14 15:37:22 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,63 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class formatted
+{
+    [JsonProperty("international")]
+    public string international { get; set; }
+
+    [JsonProperty("national")]
+    public string national { get; set; }
+
+    [JsonProperty("rfc")]
+    public string rfc { get; set; }
+
+    [JsonProperty("e164")]
+    public string e164 { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("country")]
+    public string country { get; set; }
+
+    [JsonProperty("detectedCountry")]
+    public string detectedCountry { get; set; }
+
+    [JsonProperty("countrycode")]
+    public int countrycode { get; set; }
+
+    [JsonProperty("numberNational")]
+    public int numberNational { get; set; }
+
+    [JsonProperty("extension")]
+    public object extension { get; set; }
+
+    [JsonProperty("isvalid")]
+    public bool isvalid { get; set; }
+
+    [JsonProperty("type")]
+    public string type { get; set; }
+
+    [JsonProperty("formatted")]
+    public formatted formatted { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
