f339b9640b19e4a8449fa28be38dd783dd2b5fa0
APIVerve Team <hello@apiverve.com>
2024-08-20 11:01:56 -0500

    Bug Fixes

---
+++ PhoneNumberValidatorQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class PhoneNumberValidatorQueryOptions {
+public string number { get; set; }
+public string country { get; set; }
+}
+
+}
